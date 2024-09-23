e03431435314d81fe9e7b3270e59ab6c7dd5f6d2
Gilkleyton Rodrigues <gilkleyton.andrade@gmail.com>
2024-09-23 12:48:52 -0300

    continuacao na estrutura

--- classes/Produto.php
+++ classes/Produto.php
@@ -7,6 +7,7 @@ class Produto {
     public $nome;
     public $quantidade;
     public $preco;
+    public $estoque_min;
 
     public function __construct($db) {
         $this->conn = $db;
@@ -14,18 +15,20 @@ class Produto {
 
     // Criar produto
     public function create() {
-        $query = "INSERT INTO " . $this->table . " (nome, quantidade, preco) VALUES (:nome, :quantidade, :preco)";
+        $query = "INSERT INTO " . $this->table . " (nome, quantidade, preco, estoque_min) VALUES (:nome, :quantidade, :preco, :estoque_min)";
         $stmt = $this->conn->prepare($query);
 
         // Limpar dados
         $this->nome = htmlspecialchars(strip_tags($this->nome));
         $this->quantidade = htmlspecialchars(strip_tags($this->quantidade));
         $this->preco = htmlspecialchars(strip_tags($this->preco));
+        $this->estoque_min = htmlspecialchars(strip_tags($this->estoque_min));
 
         // Bind dos dados
         $stmt->bindParam(':nome', $this->nome);
         $stmt->bindParam(':quantidade', $this->quantidade);
         $stmt->bindParam(':preco', $this->preco);
+        $stmt->bindParam(':estoque_min', $this->estoque_min);
 
         if ($stmt->execute()) {
             return true;
@@ -43,19 +46,21 @@ class Produto {
 
     // Atualizar produto
     public function update() {
-        $query = "UPDATE " . $this->table . " SET nome = :nome, quantidade = :quantidade, preco = :preco WHERE id = :id";
+        $query = "UPDATE " . $this->table . " SET nome = :nome, quantidade = :quantidade, preco = :preco, estoque_min = :estoque_min WHERE id = :id";
         $stmt = $this->conn->prepare($query);
 
         // Limpar dados
         $this->nome = htmlspecialchars(strip_tags($this->nome));
         $this->quantidade = htmlspecialchars(strip_tags($this->quantidade));
         $this->preco = htmlspecialchars(strip_tags($this->preco));
+        $this->estoque_min = htmlspecialchars(strip_tags($this->estoque_min));
         $this->id = htmlspecialchars(strip_tags($this->id));
 
         // Bind dos dados
         $stmt->bindParam(':nome', $this->nome);
         $stmt->bindParam(':quantidade', $this->quantidade);
         $stmt->bindParam(':preco', $this->preco);
+        $stmt->bindParam(':estoque_min', $this->estoque_min);
         $stmt->bindParam(':id', $this->id);
 
         if ($stmt->execute()) {
